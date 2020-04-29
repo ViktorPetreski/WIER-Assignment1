bffaa65fbc02affbb74869fd7473bfb1c1a62153
LDodevska <lodi_dodevska@yahoo.com>
2020-04-29 15:27:23 +0200

    Changed imports and relative paths

--- Assignment 2/implementation-extraction/run-extractor.py
+++ Assignment 2/implementation-extraction/run-extractor.py
@@ -1,14 +1,17 @@
 from extractors import regex_extractor, xpath_extractor
+
 from roadrunner.utils import *
+from roadrunner.roadrunner import roadrunner
 import argparse
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # parser.add_argument("algorithm", help='type of algorithm: regex, xpath, roadrunner')
     # args = parser.parse_args()
-    # regex_extractor.extract_contents("./WebPages/overstock.com/jewelry01.html")
-    # regex_extractor.extract_contents("./WebPages/overstock.com/jewelry02.html")
-    xpath_extractor.extract_content("./WebPages/overstock.com/jewelry01.html")
-    # prettify.run("./WebPages/overstock.com/jewelry01.html", "./WebPages/overstock.com/jewelry02.html")
-    # regex_extractor.extract_contents_rtvslo(
-    #     "./WebPages/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
+    # regex_extractor.extract_contents("./input-extraction/overstock.com/jewelry01.html")
+    # regex_extractor.extract_contents("./input-extraction/overstock.com/jewelry02.html")
+    # xpath_extractor.extract_content("./input-extraction/overstock.com/jewelry01.html")
+    # prettify.run("./input-extraction/WebPages/overstock.com/jewelry01.html", "./WebPages/overstock.com/jewelry02.html")
+    regex_extractor.extract_contents_rtvslo(
+        "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")

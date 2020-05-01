a889a08028de08885d5583050fd4f5121ea4f006
LDodevska <lodi_dodevska@yahoo.com>
2020-05-01 20:36:18 +0200

    Added ArgumentParser

--- Assignment 2/implementation-extraction/run-extractor.py
+++ Assignment 2/implementation-extraction/run-extractor.py
@@ -6,28 +6,38 @@ import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument("algorithm", help='type of algorithm: regex, xpath, roadrunner')
-    # args = parser.parse_args()
+    parser.add_argument("algorithm", help='type of algorithm: regex, xpath, roadrunner')
+    args = parser.parse_args()
 
-    # REGEX
+    values = vars(args)
+    if values['algorithm'] == 'regex':
+        regex_extractor.extract_contents("../input-extraction/overstock.com/jewelry01.html")
+        regex_extractor.extract_contents("../input-extraction/overstock.com/jewelry02.html")
+        regex_extractor.extract_contents_rtvslo(
+            "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
+        regex_extractor.extract_contents_rtvslo(
+            "../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html")
+        regex_extractor.extract_contents_gsc("../input-extraction/globalscaleco.com/gsc03.html")
+        regex_extractor.extract_contents_gsc("../input-extraction/globalscaleco.com/gsc04.html")
 
-    # regex_extractor.extract_contents_rtvslo(
-    #     "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
+    if values['algorithm'] == 'xpath':
+        xpath_extractor.extract_content_overstock("../input-extraction/overstock.com/jewelry01.html")
+        xpath_extractor.extract_content_overstock("../input-extraction/overstock.com/jewelry02.html")
+        xpath_extractor.extract_content_rtvslo(
+            "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
+        xpath_extractor.extract_content_rtvslo(
+            "../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html")
+        xpath_extractor.extract_content_gsc("../input-extraction/globalscaleco.com/gsc03.html")
+        xpath_extractor.extract_content_gsc("../input-extraction/globalscaleco.com/gsc04.html")
 
-    # regex_extractor.extract_contents_gsc("../input-extraction/globalscaleco.com/gsc04.html")
-
-    # XPATH
-    # xpath_extractor.extract_content_overstock("../input-extraction/overstock.com/jewelry01.html")
-    # xpath_extractor.extract_content_gsc("../input-extraction/globalscaleco.com/gsc03.html")
-    # xpath_extractor.extract_content_rtvslo(
-    #     "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
-    # xpath_extractor.extract_content_rtvslo(
-    #     "../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html")
-
-    # ROAD-RUNNER
-    sys.setrecursionlimit(3000)
-    # roadrunner.run("../input-extraction/overstock.com/jewelry01.html",
-    #                "../input-extraction/overstock.com/jewelry02.html")
-    roadrunner.run("../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html",
-                   "../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html",
-                   encoding='utf-8')
+    if values['algorithm'] == 'roadrunner':
+        sys.setrecursionlimit(3000)
+        # roadrunner.run("../input-extraction/overstock.com/jewelry01.html",
+        #                "../input-extraction/overstock.com/jewelry02.html")
+        # roadrunner.run(
+        #     "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html",
+        #     "../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html",
+        #     encoding='utf-8')
+        roadrunner.run(
+            "../input-extraction/globalscaleco.com/gsc03.html",
+            "../input-extraction/globalscaleco.com/gsc04.html", encoding='utf-8')

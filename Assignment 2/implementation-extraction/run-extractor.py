81e4527498d5ee5d3ca491930337747c6c166062
LDodevska <lodi_dodevska@yahoo.com>
2020-05-01 14:49:36 +0200

    Bug fix in utils,

--- Assignment 2/implementation-extraction/run-extractor.py
+++ Assignment 2/implementation-extraction/run-extractor.py
@@ -1,17 +1,27 @@
 from extractors import regex_extractor, xpath_extractor
-from roadrunner.roadrunner import roadrunner
+# from roadrunner.roadrunner import roadrunner
+from roadrunner import roadrunner
 import argparse
-
+import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # parser.add_argument("algorithm", help='type of algorithm: regex, xpath, roadrunner')
     # args = parser.parse_args()
-    regex_extractor.extract_contents("../input-extraction/overstock.com/jewelry01.html")
-    # regex_extractor.extract_contents("../input-extraction/overstock.com/jewelry02.html")
+
+    # REGEX
     # xpath_extractor.extract_content_overstock("../input-extraction/overstock.com/jewelry01.html")
-    # prettify.run("../input-extraction/WebPages/overstock.com/jewelry01.html", "./WebPages/overstock.com/jewelry02.html")
     # regex_extractor.extract_contents_rtvslo(
     #     "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
     # regex_extractor.extract_contents_gsc("../input-extraction/globalscaleco.com/gsc04.html")
+
+
+    # XPATH
+
     # xpath_extractor.extract_content_gsc("../input-extraction/globalscaleco.com/gsc03.html")
+
+
+    # ROAD-RUNNER
+    sys.setrecursionlimit(1500)
+    roadrunner.run("../input-extraction/overstock.com/jewelry01.html",
+                   "../input-extraction/overstock.com/jewelry02.html")
